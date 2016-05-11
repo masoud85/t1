650933ba8e8a2b3e77f47db3c1f25efd59c7b799
Masoud Ahadi <masoud.ahadi@gmail.com>
2016-05-11 16:06:34 +0430

    First Commit.

---
+++ src/main/java/com/masoud/Class1.java
@@ -0,0 +1,14 @@
+package com.masoud;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Author: Masoud
+ * Date: 5/11/16
+ * Time: 11:53 AM
+ */
+public class Class1 extends ActionSupport{
+    public String m1() {
+        return SUCCESS;
+    }
+}
